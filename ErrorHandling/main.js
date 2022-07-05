8c6263868674954e619a9057f3caabde38c8f1bf
muamarzidan <zidanprofix93@gmail.com>
2022-07-05 14:16:27 +0700

    new error

--- ErrorHandling/main.js
+++ ErrorHandling/main.js
@@ -44,54 +44,46 @@
 
 
 // TODO 1
+
 class ValidationError extends Error{
-    constructor(message){
-    super(message);
-    this.name="ValidationError";
-  }	
-}
-// TODO 2
-function validateNumberInput (a, b, c){
-  try{
-    if(typeof(a)!=='number')
-    {throw new ValidationError("Argumen pertama harus number");}
-    if(typeof(b)!=='number')
-    {throw new ValidationError("Argumen kedua harus number");}
-    if(typeof(c)!=='number')
-    {throw new ValidationError("Argumen ketiga harus number");}
-  }catch(error){
-    return `${error.message}`;
+  constructor(message){
+  super(message);
+  this.name="ValidationError";
   }
-}
-
-const detectTriangle = (a, b, c) => {
-  // TODO 3
-try{
-      const check = validateNumberInput(a,b,c);
-      if(check === undefined){
-        if (a === b && b === c) {
-    return 'Segitiga sama sisi';
   }
-
-  if (a === b || a === c || b === c) {
-    return 'Segitiga sama kaki';
-  }
-  return 'Segitiga sembarang';
-      }else{
-        const isi = validateNumberInput(a,b,c); 
-        return isi;
-      }
+  
+  // TODO 2
+  function validateNumberInput(a,b,c){
+    if(typeof(a) !== "number"){
+      throw new ValidationError("Argumen pertama harus number")
+    }else if(typeof(b) !== "number"){
+      throw new ValidationError("Argumen kedua harus number")
+    }else if(typeof(c) !== "number"){
+      throw new ValidationError("Argumen ketiga harus number")
     }
-  catch(error){
-    const isi = validateNumberInput(a,b,c); 
-        return isi;
   }
-};
-console.log(detectTriangle(1,false,1));
-console.log(detectTriangle('a', 3, 5));
-console.log(detectTriangle(12,2,null));
-
-/**
- * Jangan hapus kode di bawah ini
- */
-module.exports = { ValidationError, validateNumberInput, detectTriangle };
+  
+  const detectTriangle = (a, b, c) => {
+    // TODO 3
+       try{
+      validateNumberInput(a,b,c)
+      
+      if (a === b && b === c ) {
+        return 'Segitiga sama sisi';
+      }
+      if (a === b || a === c || b === c) {
+        return 'Segitiga sama kaki';
+      }
+      
+      return 'Segitiga sembarang';
+    }catch(error){
+      if (error instanceof ValidationError){
+        return `${error.message}`;
+      } 
+    }
+  };
+  console.log(detectTriangle(1,false,1));
+  console.log(detectTriangle('a', 3, 5));
+  console.log(detectTriangle(12,2,null));
+  
+  module.exports = { ValidationError, validateNumberInput, detectTriangle };
